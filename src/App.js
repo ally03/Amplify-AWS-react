cd7465d123262f2c871a758c50f5b03661bfa12a
Alid Didarali <aliddidarali@Alids-MacBook-Pro.local>
2019-05-05 16:32:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+import { createTodo } from "./graphql/mutations";
+// import { listNodes } from "./graphql/queries";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    note: "",
+    notes: []
+  };
+
+  // async componentDidMount() {
+  //   const result = await API.graphql(graphqlOperation(listNodes));
+  //   this.setState({ notes: result.data.listNodes.items });
+  // }
+
+  handleChangeNode = event => this.setState({ note: event.target.value });
+
+  handleAddNote = async event => {
+    const { note, notes } = this.state;
+    event.preventDefault();
+    const input = { note };
+    const result = await API.graphql(graphqlOperation(createTodo, { input }));
+    const newNote = result.data.createTodo;
+    const updatedNotes = [newNote, ...notes];
+    this.setState({ notes: updatedNotes, note: "" });
+  };
+  render() {
+    const { notes, note } = this.state;
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-1">NoteTaker</h1>
+        {/* {Note form} */}
+        <form onSubmit={this.handleAddNote} className="mb3">
+          <input
+            type="text"
+            className="pa2 f4"
+            placeholder="Write your note"
+            onChange={this.handleChangeNode}
+            value={note}
+          />
+          <button className="pa2 f4" type="submit">
+            Add Note
+          </button>
+        </form>
+
+        {/* {Note list} */}
+        <div>
+          {notes.map(item => (
+            <div key={item.id} className="flex items-center">
+              <li className="list pa1 f3">{item.note}</li>
+              <button className="bg-transparent bn f4">
+                <span>&times;</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
